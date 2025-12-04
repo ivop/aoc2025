2b48c4b4d8a3df5b4e20b36885c33e40bb0b6511
Ivo van Poorten <ivopvp@gmail.com>
2025-12-04 06:43:16 +0100

    reindent

--- day04/p2.c
+++ day04/p2.c
@@ -30,31 +30,31 @@ int main(void) {
     width = strlen(lines[0])-1;
     int answer = 0;
     bool removed;
-  do {
-    removed = false;
-    for (int y=0; y<height; y++) {
-        for (int x=0; x<width; x++) {
-            if (lines[y][x] == '.') continue;
-            int cnt = 0;
-            if (y>0) {
-                if (x>0       && lines[y-1][x-1] == '@') cnt++;
-                if (             lines[y-1][x+0] == '@') cnt++;
-                if (x<width-1 && lines[y-1][x+1] == '@') cnt++;
-            }
-            if (x>0       && lines[y][x-1] == '@') cnt++;
-            if (x<width-1 && lines[y][x+1] == '@') cnt++;
-            if (y<width-1) {
-                if (x>0       && lines[y+1][x-1] == '@') cnt++;
-                if (             lines[y+1][x+0] == '@') cnt++;
-                if (x<width-1 && lines[y+1][x+1] == '@') cnt++;
-            }
-            if (cnt < 4) {
-                answer++;
-                lines[y][x] = '.';
-                removed = true;
+    do {
+        removed = false;
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                if (lines[y][x] == '.') continue;
+                int cnt = 0;
+                if (y>0) {
+                    if (x>0       && lines[y-1][x-1] == '@') cnt++;
+                    if (             lines[y-1][x+0] == '@') cnt++;
+                    if (x<width-1 && lines[y-1][x+1] == '@') cnt++;
+                }
+                if (x>0       && lines[y][x-1] == '@') cnt++;
+                if (x<width-1 && lines[y][x+1] == '@') cnt++;
+                if (y<width-1) {
+                    if (x>0       && lines[y+1][x-1] == '@') cnt++;
+                    if (             lines[y+1][x+0] == '@') cnt++;
+                    if (x<width-1 && lines[y+1][x+1] == '@') cnt++;
+                }
+                if (cnt < 4) {
+                    answer++;
+                    lines[y][x] = '.';
+                    removed = true;
+                }
             }
         }
-    }
-  } while (removed);
+    } while (removed);
     printf("%d\n", answer);
 }
