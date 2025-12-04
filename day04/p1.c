b444453b8fb28f0ef6c16a33ddc523f011a9f924
Ivo van Poorten <ivopvp@gmail.com>
2025-12-04 06:41:36 +0100

    day 04

---
+++ day04/p1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <unistd.h>
+
+char **lines;
+int nlines;
+int space;
+
+char *line;
+size_t n;
+
+int height;
+int width;
+
+int main(void) {
+    while (getline(&line, &n, stdin) > 0) {
+        if (nlines == space) {
+            space += 16;
+            lines = realloc(lines, space * sizeof(char*));
+        }
+        lines[nlines] = strdup(line);
+        nlines++;
+    }
+    height = nlines;
+    width = strlen(lines[0])-1;
+    int answer = 0;
+    for (int y=0; y<height; y++) {
+        for (int x=0; x<width; x++) {
+            if (lines[y][x] == '.') continue;
+            int cnt = 0;
+            if (y>0) {
+                if (x>0       && lines[y-1][x-1] == '@') cnt++;
+                if (             lines[y-1][x+0] == '@') cnt++;
+                if (x<width-1 && lines[y-1][x+1] == '@') cnt++;
+            }
+            if (x>0       && lines[y][x-1] == '@') cnt++;
+            if (x<width-1 && lines[y][x+1] == '@') cnt++;
+            if (y<width-1) {
+                if (x>0       && lines[y+1][x-1] == '@') cnt++;
+                if (             lines[y+1][x+0] == '@') cnt++;
+                if (x<width-1 && lines[y+1][x+1] == '@') cnt++;
+            }
+            if (cnt < 4) answer++;
+        }
+    }
+    printf("%d\n", answer);
+}
